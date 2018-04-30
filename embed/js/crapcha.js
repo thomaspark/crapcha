3b14353e78fe724a115c623642ba090a63c46050
Thomas Park <thomas@thomaspark.co>
2018-04-29 23:06:10 -0400

    convert tabs to spaces

--- embed/js/crapcha.js
+++ embed/js/crapcha.js
@@ -1,129 +1,129 @@
 var crapcha = {
 
-	init: function($el) {
+  init: function($el) {
 
-		var html = '<div class="code"></div><div class="control"><input type="text" maxlength="20" placeholder="Type the text above"></input><label><span class="icon-021 reload btn"></span><a class="icon-05a info btn" href="http://crapcha.com" target="_blank" tabindex="-1"></a> <span class="logo">CRAPCHA<sup>&trade;</sup></span></label></div>';
+    var html = '<div class="code"></div><div class="control"><input type="text" maxlength="20" placeholder="Type the text above"></input><label><span class="icon-021 reload btn"></span><a class="icon-05a info btn" href="http://crapcha.com" target="_blank" tabindex="-1"></a> <span class="logo">CRAPCHA<sup>&trade;</sup></span></label></div>';
 
-		$el.append(html)
-			.children('.control')
-				.children('input').keypress(this.send).end()
-				.find('.reload').css('user-select', 'none').click(function(){
-					crapcha.populate($el);
-				});
+    $el.append(html)
+      .children('.control')
+        .children('input').keypress(this.send).end()
+        .find('.reload').css('user-select', 'none').click(function(){
+          crapcha.populate($el);
+        });
 
-		this.populate($el);
+    this.populate($el);
 
-	  var config = {
-	    apiKey: "AIzaSyAvlVi3PMdwBXjAvUrGEtPj2wnQGvzkHEY",
-	    authDomain: "crapcha-7bee7.firebaseapp.com",
-	    databaseURL: "https://crapcha-7bee7.firebaseio.com",
-	    projectId: "crapcha-7bee7",
-	    storageBucket: "crapcha-7bee7.appspot.com",
-	    messagingSenderId: "354184007766"
-	  };
-	  firebase.initializeApp(config);
-	},
+    var config = {
+      apiKey: "AIzaSyAvlVi3PMdwBXjAvUrGEtPj2wnQGvzkHEY",
+      authDomain: "crapcha-7bee7.firebaseapp.com",
+      databaseURL: "https://crapcha-7bee7.firebaseio.com",
+      projectId: "crapcha-7bee7",
+      storageBucket: "crapcha-7bee7.appspot.com",
+      messagingSenderId: "354184007766"
+    };
+    firebase.initializeApp(config);
+  },
 
-	populate: function($el) {
+  populate: function($el) {
 
-		var $code = $el.children('.code'),
-			captcha = this.generate(5);
+    var $code = $el.children('.code'),
+      captcha = this.generate(5);
 
-		$code.empty();
+    $code.empty();
 
-		for (var i = 0; i < captcha.length; i++) {
+    for (var i = 0; i < captcha.length; i++) {
 
-			if (captcha[i].length === 5) {
-				// unicode
-				$code.append('<span>&#x' + captcha[i] + ';</span>');
-			} else if (captcha[i].length === 3) {
-				// font-awesome
-				$code.append('<span class="icon-' + captcha[i] + '"></span>');
-			}
-		}
+      if (captcha[i].length === 5) {
+        // unicode
+        $code.append('<span>&#x' + captcha[i] + ';</span>');
+      } else if (captcha[i].length === 3) {
+        // font-awesome
+        $code.append('<span class="icon-' + captcha[i] + '"></span>');
+      }
+    }
 
-		$code.children().each(this.distort);
-	},
+    $code.children().each(this.distort);
+  },
 
-	generate: function(length) {
+  generate: function(length) {
 
-		var captcha = [],
-			t,
-			r;
+    var captcha = [],
+      t,
+      r;
 
-		for (var i = 0; i < length; i++) {
+    for (var i = 0; i < length; i++) {
 
-			r = Math.random();
+      r = Math.random();
 
-			if (r > 0.4) {
-				// unicode
-				t = this.randomize(382).toString(16);
-				t = this.pad([t, 5]);
-			} else if (r > 0.2) {
-				// unicode
-				t = this.randomize(12351).toString(16);
-				t = this.pad([t, 5]);
-			} else {
-				// font-awesome
-				t = this.randomize(270).toString(15);
-				t = this.pad([t, 3]);
-			}
+      if (r > 0.4) {
+        // unicode
+        t = this.randomize(382).toString(16);
+        t = this.pad([t, 5]);
+      } else if (r > 0.2) {
+        // unicode
+        t = this.randomize(12351).toString(16);
+        t = this.pad([t, 5]);
+      } else {
+        // font-awesome
+        t = this.randomize(270).toString(15);
+        t = this.pad([t, 3]);
+      }
 
-			captcha.push(t);
-		}
+      captcha.push(t);
+    }
 
-		return captcha;
-	},
+    return captcha;
+  },
 
-	distort: function(i) {
+  distort: function(i) {
 
-		var length = $(this).parent().children().length,
-			size = parseInt($(this).parent().height(), 10);
+    var length = $(this).parent().children().length,
+      size = parseInt($(this).parent().height(), 10);
 
-		$(this).css({
-			'left': 8 + (100 * i / length) + '%',
-			'position': 'absolute',
-			'font-size': size - crapcha.randomize(20) + 'px',
-			'transform': 'rotateY(' + crapcha.randomize(70) + 'deg) rotate(' + crapcha.randomize(360) + 'deg)',
-			'user-select': 'none'
-		});
-	},
+    $(this).css({
+      'left': 8 + (100 * i / length) + '%',
+      'position': 'absolute',
+      'font-size': size - crapcha.randomize(20) + 'px',
+      'transform': 'rotateY(' + crapcha.randomize(70) + 'deg) rotate(' + crapcha.randomize(360) + 'deg)',
+      'user-select': 'none'
+    });
+  },
 
-	send: function(e) {
+  send: function(e) {
 
-		if (e.which == 13) {
+    if (e.which == 13) {
 
-			if (($(this).val()) && ($(this).val() !== crapcha.attempt)) {
+      if (($(this).val()) && ($(this).val() !== crapcha.attempt)) {
 
-				crapcha.attempt = $(this).val().substring(0, 20);
-				var $captcha = $(this).parent().parent().children('.code');
+        crapcha.attempt = $(this).val().substring(0, 20);
+        var $captcha = $(this).parent().parent().children('.code');
 
-				if (($captcha.children('span').size() === $captcha.find('*').size()) && ($captcha.find('[onload], [onunload], [onclick], [onmouseover], [onmouseout], [onmousedown], [onmouseup]').size() === 0)) {
+        if (($captcha.children('span').size() === $captcha.find('*').size()) && ($captcha.find('[onload], [onunload], [onclick], [onmouseover], [onmouseout], [onmousedown], [onmouseup]').size() === 0)) {
 
-  				var database = firebase.database();
+          var database = firebase.database();
 
-  				database.ref("attempts").push().set({
-  					captcha: $captcha.html(),
-  					attempt: crapcha.attempt,
-  					timestamp: firebase.database.ServerValue.TIMESTAMP
-  				});
-				}
+          database.ref("attempts").push().set({
+            captcha: $captcha.html(),
+            attempt: crapcha.attempt,
+            timestamp: firebase.database.ServerValue.TIMESTAMP
+          });
+        }
 
-				$(this).val('');
+        $(this).val('');
 
-				crapcha.populate($(this).parent().parent());
-			}
-		}
-	},
+        crapcha.populate($(this).parent().parent());
+      }
+    }
+  },
 
-	randomize: function(max) {
-		return Math.floor((Math.random()) * max);
-	},
+  randomize: function(max) {
+    return Math.floor((Math.random()) * max);
+  },
 
-	pad: function(arg) {
-		var s = '00000' + arg[0];
-		return s.substr(s.length - arg[1]);
-	}
+  pad: function(arg) {
+    var s = '00000' + arg[0];
+    return s.substr(s.length - arg[1]);
+  }
 };
 
 crapcha.init($('.crapcha'));
